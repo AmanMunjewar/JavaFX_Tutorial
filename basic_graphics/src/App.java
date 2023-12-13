15fb669197c9d1a58538eb3d85a13384749b5996
AMAN MUNJEWAR <munjewaraman07@gmail.com>
2023-12-13 09:06:09 +0530

    Text graphics added.

--- basic_graphics/src/App.java
+++ basic_graphics/src/App.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -10,5 +11,19 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root);
+
+        Text text = new Text();
+        text.setText("Hello there");
+        text.setX(50);
+        text.setY(50);
+
+        root.getChildren().addAll(text);
+
+        primaryStage.setScene(scene);
+        primaryStage.setHeight(800);
+        primaryStage.setWidth(900);
+        primaryStage.show();
     }
 }
