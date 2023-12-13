a7c5c25cb16d5895cff0afd1038f1cdcf90d84e3
AMAN MUNJEWAR <munjewaraman07@gmail.com>
2023-12-13 09:53:35 +0530

    Completed polygon and rectangle graphics.

--- basic_graphics/src/App.java
+++ basic_graphics/src/App.java
@@ -1,9 +1,12 @@
+import javax.swing.text.TextAction;
+
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -40,11 +43,37 @@ public class App extends Application {
         line.setEndY(150);
         line.setStrokeWidth(3);
         line.setStroke(Color.web("#4B56D2"));
-        line.setOpacity(0.2);
+        line.setOpacity(0.8);
 
-        
+        Text rectanglText = new Text("2) Rectangle");
+        rectanglText.setX(50);
+        rectanglText.setY(180);
+        rectanglText.setFont(defaulFont);
+        rectanglText.setFill(line_text.getFill());
 
-        root.getChildren().addAll(text, line_text, line);
+        Rectangle rectangle = new Rectangle(50, 210, 300, 70);
+        rectangle.setFill(Color.web("#4B56D2"));
+        rectangle.setOpacity(0.8);
+        rectangle.setStroke(line_text.getFill());
+        rectangle.setStrokeWidth(4);
+
+        Text polygon_text = new Text(50, 320, "3) Polygon");
+        polygon_text.setFont(defaulFont);
+        polygon_text.setFill(line_text.getFill());
+
+        Polygon polygon = new Polygon();
+        polygon.getPoints().addAll(
+            50.0, 330.0,
+            50.0, 450.0,
+            250.0, 350.0
+        );
+        polygon.setFill(Color.web("#4B56D2"));
+        polygon.setOpacity(0.8);
+        polygon.setStroke(line_text.getFill());
+        polygon.setStrokeWidth(4);
+
+        root.getChildren().addAll(text, line_text, line, rectanglText, rectangle, 
+        polygon_text, polygon);
 
         primaryStage.setScene(scene);
         primaryStage.setHeight(800);
