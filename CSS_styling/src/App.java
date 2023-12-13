904a152a8a2fe3185d02153442b0eb7d5952e203
AMAN MUNJEWAR <munjewaraman07@gmail.com>
2023-12-13 12:43:20 +0530

    Working day and night mode.

--- CSS_styling/src/App.java
+++ CSS_styling/src/App.java
@@ -1,9 +1,7 @@
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -15,7 +13,9 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
         Scene scene = new Scene(root, 600, 500);
-
+        String css = this.getClass().getResource("night_mode.css").toExternalForm();
+        String css2 = this.getClass().getResource("day_mode.css").toExternalForm();
+        scene.getStylesheets().addAll(css, css2);
 
         primaryStage.setTitle("CSS styling");
         primaryStage.setScene(scene);
---
+++ CSS_styling/src/Controller.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Controller {
+
+    @FXML
+    private Button day_night;
+    private boolean swap=true;
+
+    public void change_color(ActionEvent event) throws IOException{
+        if (swap) {
+            swap=false;
+            day_night.setText("Night");
+
+            Scene scene = ((Node)event.getSource()).getScene();
+            String css = this.getClass().getResource("day_mode.css").toExternalForm();
+            scene.getStylesheets().clear();
+            scene.getStylesheets().add(css);
+
+            
+        } else {
+            swap=true;
+            day_night.setText("Day");
+            
+            Scene scene = ((Node)event.getSource()).getScene();
+            String css = this.getClass().getResource("night_mode.css").toExternalForm();
+            scene.getStylesheets().clear();
+            scene.getStylesheets().add(css);
+        }
+    }
+    
+}
