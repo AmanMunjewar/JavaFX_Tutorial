db852d059da33d159fcaff49cbd3e4fda89c86f9
AMAN MUNJEWAR <munjewaraman07@gmail.com>
2023-12-13 09:23:01 +0530

    line graphics implemented.

--- basic_graphics/src/App.java
+++ basic_graphics/src/App.java
@@ -1,6 +1,10 @@
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -12,14 +16,35 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Group root = new Group();
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, Color.web("#F1F6F5"));
+
+        Font defaulFont = Font.font("Lucida Console", 30);
 
         Text text = new Text();
         text.setText("Hello there");
         text.setX(50);
         text.setY(50);
+        text.setFont(defaulFont);
+        text.setFill(Color.web("#82C3EC"));
 
-        root.getChildren().addAll(text);
+        Text line_text = new Text("1) Line");
+        line_text.setX(50);
+        line_text.setY(90);
+        line_text.setFont(defaulFont);
+        line_text.setFill(Color.web("82C3EC"));
+
+        Line line = new Line();
+        line.setStartX(50);
+        line.setStartY(110);
+        line.setEndX(600);
+        line.setEndY(150);
+        line.setStrokeWidth(3);
+        line.setStroke(Color.web("#4B56D2"));
+        line.setOpacity(0.2);
+
+        
+
+        root.getChildren().addAll(text, line_text, line);
 
         primaryStage.setScene(scene);
         primaryStage.setHeight(800);
