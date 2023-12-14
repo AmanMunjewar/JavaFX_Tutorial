13cf0eb55fd76efe1cded363412420c12e18d854
AMAN MUNJEWAR <munjewaraman07@gmail.com>
2023-12-14 09:32:40 +0530

    Some changes to color scheme.

--- CSS_styling/src/Controller.java
+++ CSS_styling/src/Controller.java
@@ -2,18 +2,14 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
 
 public class Controller {
 
     @FXML
     private Button day_night;
-    private boolean swap=true;
 
     public void change_color(ActionEvent event) throws IOException{
 
