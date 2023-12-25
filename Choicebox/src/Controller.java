852961d0a44606c44cc14e2835d3861695f8dc80
AMAN MUNJEWAR <munjewaraman07@gmail.com>
2023-12-25 11:53:27 +0530

    Finalized choicebox.

--- Choicebox/src/Controller.java
+++ Choicebox/src/Controller.java
@@ -1,18 +1,27 @@
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 
 public class Controller implements Initializable{
     @FXML
     ChoiceBox<String> myChoiceBox;
+    @FXML
+    Label myLabel;
 
     private String food[] = {"pizza", "burger", "fries"};
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         myChoiceBox.getItems().addAll(food);
-    }    
+        myChoiceBox.setOnAction(this::choice);
+    }   
+
+    public void choice(ActionEvent event){
+        myLabel.setText(myChoiceBox.getValue());
+    }
 }
